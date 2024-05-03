1e418215df7765c8f0090c757f91118bfffee893
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-03 14:35:07 +0530

    chore: Update .prettierrc and eslint.config.mjs
    - Update .prettierrc to include parser configuration for typescript
    - Update eslint.config.mjs to include eslint-plugin-prettier
    - Fix formatting in server.ts
    - Fix missing semicolon in server.ts
    - Update eslint.config.mjs with eslint-config-prettier
    - Update devDependencies in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,19 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default {
-  languageOptions: { globals: globals.browser },
-
-  extends: [
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
-
-  plugins: ['@typescript-eslint', 'prettier'],
-
-  rules: {
-    'prettier/prettier': 'error',
-  },
-}
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.eslintRecommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    languageOptions : {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+      }
+    }
+  }
+]
