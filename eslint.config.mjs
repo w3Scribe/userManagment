6b8cdff90d0f2b6b4081169caff0fd569ee7c717
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-03 15:18:29 +0530

    chore: Update devDependencies in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,10 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.eslintRecommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  {
-    languageOptions : {
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname
-      }
-    }
-  }
-]
+  ...tseslint.configs.recommended,
+];
