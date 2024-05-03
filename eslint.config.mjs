2c22ec31d2376359713eeaee19caaed63856ee1a
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-03 10:54:22 +0530

    chore: Update eslint.config.mjs to include eslint-plugin-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint, { plugin } from 'typescript-eslint'
+import { eslintConfigPrettier } from 'eslint-config-prettier'
+import eslintPrettierPlugin from 'eslint-plugin-prettier'
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-];
+export default {
+  languageOptions: { globals: globals.browser },
+  extends: [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPrettierPlugin,
+    eslintConfigPrettier,
+  ],
+  
+  plugins: ["@eslint/js", 'prettier'],
+
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
