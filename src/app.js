7d7ac8f8f8e2021e5640fbeea7ee5d339e33c2a7
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-05 11:05:26 +0530

    refactor: Update user routes and improve code organization
    - Move user routes from app.js to separate user.js file
    - Update userRouter in user.js to handle GET requests for all users and specific user by name
    - Remove unused import and function in user.js
    - Update app.js to use the userRouter for /api/users route

--- src/app.js
+++ src/app.js
@@ -1,36 +1,16 @@
 import express from 'express'
-import createHttpError from 'http-errors'
 import bodyParser from './middlewares/bodyParser.js'
 import errorHandler from './middlewares/errorHandler.js'
+import userRouter from "./routes/user.js"
+
+// express app
 const app = express()
 
 // body parser middleware
 bodyParser(app)
 
-const userData = [
-	{ id: 1, name: 'sudhir', lastname: 'gadpayle' },
-	{ id: 2, name: 'samir', lastname: 'zode' },
-	{ id: 3, name: 'sagar', lastname: 'gadpayle' }
-]
-
-app.get('/', (_req, _res, next) => {
-	next(createHttpError(404, 'user not found.'))
-})
-
-app.get('/api/user', (_req, res) => {
-	res.status(202).json(userData)
-})
-
-app.get('/api/user/:name', (req, res, next) => {
-	const { name } = req.params
-
-	const user = userData.filter(
-		user => user.name.includes(name) || user.lastname.includes(name)
-	)
-
-	if (user) return res.status(200).json(user)
-	next(createHttpError(404, 'user not found.'))
-})
+// routes
+app.use("/api/users", userRouter)
 
 // global error handler Middleware
 app.use(errorHandler)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,31 @@
-import {Router} from "express";
+import { Router } from 'express'
+import createHttpError from 'http-errors'
 
-const userRouter = Router();
+const userRouter = Router()
 
-userRouter.get("/", ())
+const userData = [
+	{ id: 1, name: 'sudhir', lastname: 'gadpayle' },
+	{ id: 2, name: 'samir', lastname: 'zode' },
+	{ id: 3, name: 'sagar', lastname: 'gadpayle' }
+]
+
+
+userRouter.get('/', (_req, res, _next) => {
+	res.status(202).json(userData)
+})
+
+
+userRouter.get('/:name', (req, res, next) => {
+	const { name } = req.params
+
+	const user = userData.filter(
+		user => user.name.includes(name) || user.lastname.includes(name)
+	)
+
+	if (user) return res.status(200).json(user)
+	next(createHttpError(404, 'user not found.'))
+})
+
+
 
 export default userRouter
