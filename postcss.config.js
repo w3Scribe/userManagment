0894a98c443ea6f2744e1cdcc12ef2002a65ffef
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-08 19:50:40 +0530

    configured the postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+/**
+ * PostCSS configuration file
+ * @file   postcss.config.js
+ * @module tailwindcss  
+ * @module autoprefixer
+ * @module cssnano
+ */
+
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
