7e5ebc86dabc34d3a1d7c1cfca64e6dde4694e6d
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-10 17:25:52 +0530

    refactor: Add browser-sync for live reloading and CSS injection
    This commit adds the browser-sync npm package as a devDependency in the package.json file. It also imports browser-sync in the server.js file and initializes it to enable live reloading and CSS injection. This change improves the developer experience by automatically refreshing the browser when changes are made to the CSS files, providing a faster feedback loop during development.

--- server.js
+++ server.js
@@ -7,22 +7,48 @@
  * @overview    Entry point of the application where the server is started.
  */
 
+import browserSync from 'browser-sync'
 import app from './src/app.js'
 import config from './src/config/variables.js'
 import chalk from 'chalk'
+import { execSync } from 'node:child_process'
+import { join } from 'node:path'
+
+const __filename = import.meta.url
+console.log(__filename)
 
 // environment PORTS
 const PORT = config.PORT
 
-// start server
-app.listen(PORT, () => {  
-	console.log(
-		chalk.green(`Server is running on port http://localhost:${PORT}`)
-	)
+browserSync({		
+	browser: 'default',
+	port: process.env.PORT,
+	listen: CURRENT_RUNNING_URL,
+	files: join(__dirname, 'public', 'css', 'styles.css')
 })
 
-// error handling
-app.on('error', error => {
-	console.error(chalk.red(`Error: ${error}`))
-	process.exit(1)
-})
+// start server
+function server(PORT) {
+	const CURRENT_RUNNING_URL = `http://localhost:${PORT}`
+
+	app.listen(PORT, () => {
+		console.log(
+			chalk.green(`Server is running on PORT ${CURRENT_RUNNING_URL}`)
+		)
+	})
+
+	app.on('error', err => {
+		console.log(chalk.red(`Error: ${err}`))
+	})
+
+	try {
+		execSync(
+			`browser-sync start --proxy ${CURRENT_RUNNING_URL} --files "cs/*.csss"`
+		)
+	} catch (error) {
+		console.error('Failed to start browser-sync:', error)
+	}
+
+}
+
+server(PORT)
