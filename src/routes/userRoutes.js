68e6f132f4443c9dc704491b9cd5d4089ccfc71d
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-14 13:08:49 +0530

    refactor: Update userRoutes.js to improve route handling
    This commit updates the userRoutes.js file to improve the route handling. It replaces the deprecated res.render method with res.send to send a welcome message to the user when accessing the "/" route. This change simplifies the code and improves the response sent to the client.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,30 +5,12 @@
  * @description This is the user router module that handles all the user routes
  */
 
-import { Router } from 'express'
+import { Router } from "express";
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.get('/', (_req, res) => {
-   res.status(200).render('index', { title: 'User'})
-})
+userRouter.get("/", (_req, res) => {
+  res.send("Welcome to the user route");
+});
 
-userRouter.post('/', (_req, res) => {
-   res.status(200).render('index', { title: 'User'})
-})
-
-userRouter.put('/', (_req, res) => {
-   res.status(200).render('index', { title: 'User'})
-})
-
-userRouter.patch('/', (_req, res) => {
-   res.status(200).render('index', { title: 'User'})
-})
-
-userRouter.delete('/', (_req, res) => {
-   res.status(200).render('index', { title: 'User'})
-})
-
-
-
-export default userRouter  
+export default userRouter;
