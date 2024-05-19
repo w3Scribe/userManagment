c7c0bc197a5752498389a8b5ea1983a818901207
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-19 08:29:18 +0530

    refactor: Improve loginController error handling
    This commit improves the error handling in the loginController function in loginUser.js. It adds a check for invalid credentials and throws a 401 error with the message "Invalid credentials" if the user or password is not valid. This change enhances the security and user experience of the login process.

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -7,20 +7,16 @@ async function loginController(req, res, next) {
   const { email, password } = req.body;
   try {
     const user = await userModel.findOne({ email });
-
-    if (!user) {
-      throw createHttpError(401, "Invalid credentials");
-    }
+    if (!user) throw createHttpError(401, "Invalid credentials");
 
     const isPasswordValid = await verifyPassword(password, user.password);
-
-    if (!isPasswordValid) {
-      throw createHttpError(401, "Password is incorrect");
-    }
-
+    if (!isPasswordValid) throw createHttpError(401, "Password is incorrect");
+    
     const token = generateToken({ email: user.email }, "1m");
     res.cookie("token", token, { httpOnly: true, maxAge: 1000 * 60 * 4 });
+
     return res.redirect("/api/users");
+
   } catch (error) {
     next(error);
   }
