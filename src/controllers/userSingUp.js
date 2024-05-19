0ee4c9e0d9e7d39c31dbe3c68e18220af205d957
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-05-19 11:41:41 +0530

    refactor: Improve user signup controller
    The code changes in the `userSingUp` function improve the user signup controller. This commit refactors the code to handle user sign up by extracting the `username`, `email`, and `password` from the request body. It also adds error handling and validation for the signup process. This change enhances the functionality and security of the user signup feature.

--- src/controllers/userSingUp.js
+++ src/controllers/userSingUp.js
@@ -2,6 +2,15 @@ import userModel from "../models/userModel.js";
 import chalk from "chalk";
 import { generateToken } from "../utils/generateToken.js";
 
+/**
+ * Handles user sign up.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Promise<void>} - A promise that resolves when the sign up process is complete.
+ */
+
 async function userSingUp(req, res, next) {
   const { username, email, password } = req.body;
   try {
